4739288bffee0a44070437ecfa65c6999f9281c3
ikuma <chemicalbondo@gmail.com>
2022-11-17 17:56:22 +0900

    レート取得をyfinanceからapi.exchangerate.hostに変更

--- fx.py
+++ fx.py
@@ -1,13 +1,28 @@
 import argparse
 
-import yfinance
+import requests
 
-parser = argparse.ArgumentParser(description="通貨ペアの為替計算を行います")
+url = 'https://api.exchangerate.host/latest'
+
+helpdoc = f"通貨ペアの為替計算を行います。{url}"
+parser = argparse.ArgumentParser(description=helpdoc)
 parser.add_argument("base", type=str, help="基準通貨(USD, JPYなど)")
 parser.add_argument("conv", type=str, help="変換通貨(同上)")
 parser.add_argument("price", type=int, help="基準通貨の金額")
 args = parser.parse_args()
 
-data = yfinance.download(f"{args.base}{args.conv}=X", period="1d", interval="1m", rounding=True, progress=False)
-fx_price = data.Close[-1:].max() * args.price
-print(f"{int(args.price)} {args.base.upper()} = {fx_price} {args.conv.upper()}")
+params = {
+    "from": args.base.upper(),
+    "to": args.conv.upper(),
+    "amount": args.price,
+}
+request_url = 'https://api.exchangerate.host/convert'
+responce = requests.get(request_url, params=params)
+data = responce.json()
+
+if data["success"]:
+    result_price = data["result"]
+    print(f"{int(args.price)} {args.base.upper()} = {result_price} {args.conv.upper()}")
+    print(responce.url)
+else:
+    print("Failed.")
